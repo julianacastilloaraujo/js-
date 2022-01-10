5a5c78c408cb97a327fb9ff73792b1d55039f348
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-01-10 16:19:13 -0500

    Updated

---
+++ Variables.js
@@ -0,0 +1,19 @@
+var nombre = "Juliana";
+
+// declarar
+var edad;
+
+edad
+//inicializar
+edad = 20;
+
+edad
+
+var elementos = ["Computadora", "Celular"]; 
+
+var persona = {
+    nombre: "Juliana",
+    edad: 19
+}
+
+persona;

c0630b427ac177b4c5af6a930bd31df6569e8fa2
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-01-10 16:18:34 -0500

    Updated

---
+++ Funciones.js
@@ -0,0 +1,15 @@
+//Funciones Declarativas
+
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+//Funciones Expresión o Funciones Anonimas
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
